3df470f35227c5220f54fe7f964c2f80f1655aba
Isaac Yanez <isaacyanez@Isaacs-MBP-2.lan>
2022-04-19 23:08:02 -0500

    added more buttons to the cards I also added the the links behind the buttons to be routed to said page

--- src/Pages/Projects/index.js
+++ src/Pages/Projects/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Stack } from 'react-bootstrap'
 import Image from '../../Assets/isaac-yanez-background.png'
 
 function Projects () {
@@ -13,16 +13,18 @@ function Projects () {
                 </div>
 
 <div className="row my-5 align-items-center justify-content-center g-3">
-    <div className="col-8 col-lg-4 col-xl-3">
+<div className="col-8 col-lg-4 col-xl-3">
         <Card className="border-0">
     <Card.Body className="text-center py-4">
-        <Card.Title><h4>Card Title</h4></Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                <Card.Text className="mx-5">
+        <Card.Title><h4>Paw walkers</h4></Card.Title>
+                <Card.Text className="mx-5 d-none d-lg-block">
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
                 </Card.Text>
-                <Button className="btn-outline btn-lg mt-3">Go somewhere</Button>
+                <Stack gap={1} className="col-md-6 mx-auto">
+                <Button className="btn-outline btn-md mt-3" href="https://iyanez2314-dogwalking.herokuapp.com/login" target="_blank">Live website</Button>
+                <Button className="btn-outline btn-md mt-3" href="https://github.com/iyanez2314/dog-walkers" target="_blank">Code</Button>
+                </Stack>
     </Card.Body>
     </Card>
     </div>
@@ -30,13 +32,15 @@ function Projects () {
     <div className="col-8 col-lg-4 col-xl-3">
         <Card className="border-0">
     <Card.Body className="text-center py-4">
-        <Card.Title><h4>Card Title</h4></Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title><h4>GoGo Gomez</h4></Card.Title>
                 <Card.Text className="mx-5 d-none d-lg-block">
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
                 </Card.Text>
-                <Button className="btn-outline btn-lg mt-3">Go somewhere</Button>
+                <Stack gap={1} className="col-md-6 mx-auto">
+                <Button className="btn-outline btn-md mt-3">Live website</Button>
+                <Button className="btn-outline btn-md mt-3" href="https://github.com/chellesjord/gogogomez" target="_blank">Code</Button>
+                </Stack>
     </Card.Body>
     </Card>
     </div>
@@ -44,13 +48,15 @@ function Projects () {
     <div className="col-8 col-lg-4 col-xl-3">
         <Card className="border-0">
     <Card.Body className="text-center py-4">
-        <Card.Title><h4>Card Title</h4></Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Title><h4>My Anime</h4></Card.Title>
                 <Card.Text className="mx-5 d-none d-lg-block">
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
                 </Card.Text>
-                <Button className="btn-outline btn-lg mt-3">Go somewhere</Button>
+                <Stack gap={1} className="col-md-6 mx-auto">
+                <Button className="btn-outline btn-md mt-3" href="https://ethanrugh.github.io/project-1/" target="_blank">Live website</Button>
+                <Button className="btn-outline btn-md mt-3" href="https://github.com/EthanRugh/project-1" target="_blank">Code</Button>
+                </Stack>
     </Card.Body>
     </Card>
     </div>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Container, Offcanvas, NavDropdown, FormControl, Form, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 
 function Navigation () {
