24ea2a2110f6347a9d19a677b7efc769c11064c5
Isaac Yanez <isaacyanez@Isaacs-MBP-2.lan>
2022-05-02 22:33:47 -0500

    added some text to the projects that I had, I also removed some text that made the cards alittle to big

--- src/Pages/Projects/index.js
+++ src/Pages/Projects/index.js
@@ -17,9 +17,8 @@ function Projects () {
         <Card className="border-0">
     <Card.Body className="text-center py-4">
         <Card.Title><h4>Paw walkers</h4></Card.Title>
-                <Card.Text className="mx-5 d-none d-lg-block">
-                Paw walkers is a full-stack website that was built with a few of my classmates. Some of the technologies that were used were MySQL, Node.js, Sequelize ORM, Bulma CSS as well as custom API with CRUD operations. For future development, we would like to create a dashboard for the owners to add or delete pets associated with them as well as book appointments with Paw Walkers.
-                </Card.Text>
+        <Card.Subtitle className="mb-2 text-muted py-2">Technologies used</Card.Subtitle>
+        <Card.Subtitle className="mb-2 py-2 ">Handlebars.js, MySQL, Sequelize ORM, Bulma, and custom API using CRUD operations</Card.Subtitle>
                 <Stack gap={1} className="col-md-6 mx-auto">
                 <Button className="btn-outline btn-md mt-3" href="https://iyanez2314-dogwalking.herokuapp.com/login" target="_blank">Live website</Button>
                 <Button className="btn-outline btn-md mt-3" href="https://github.com/iyanez2314/dog-walkers" target="_blank">Code</Button>
@@ -32,9 +31,8 @@ function Projects () {
         <Card className="border-0">
     <Card.Body className="text-center py-4">
         <Card.Title><h4>My Anime</h4></Card.Title>
-                <Card.Text className="mx-5 d-none d-lg-block">
-                My Anime Finder was built by a few of my fellow classmates as well as me. This website is a reverse image search for any anime photo. As well as a info seach on any anime that the user inputs.
-                </Card.Text>
+        <Card.Subtitle className="mb-2 text-muted py-2">Technologies used</Card.Subtitle>
+        <Card.Subtitle className="mb-2 py-2 ">HTML, JavaScript, CSS, 3rd party API's</Card.Subtitle>
                 <Stack gap={1} className="col-md-6 mx-auto">
                 <Button className="btn-outline btn-md mt-3" href="https://ethanrugh.github.io/project-1/" target="_blank">Live website</Button>
                 <Button className="btn-outline btn-md mt-3" href="https://github.com/EthanRugh/project-1" target="_blank">Code</Button>
